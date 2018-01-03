ff9df8d51e7a591e792590a53842297428ee40c5
Nitin Pamnani <nitin.pamnani@evolphin.com>
2018-01-03 20:50:21 +0530

    prize selection change

--- view/viewleaderboard.php
+++ view/viewleaderboard.php
@@ -69,8 +69,8 @@
                       <div class="card" style="width: 20rem;">
                           <img class="card-img-top" src="http://www.thebigwhammy.com/assets/images/default.jpg" alt="Card image cap">
                           <div class="card-block">
-                              <h4 class="card-title">Everest</h4>
-                              <p class="card-text">Everest Award is given to the player who achieved the highest score in a month, in any game week.</p>
+                              <h4 class="card-title">Iron Man</h4>
+                              <p class="card-text">Stay Tuned. Will be updated shortly.</p>
 
                           </div>
                       </div>
@@ -79,8 +79,15 @@
 
 
                   </div><!-- /.col-sm-4 -->
-                  <div class="col-sm-1">
+                  <div class="col-sm-4">
+					<div class="card" style="width: 20rem;">
+                          <img class="card-img-top" src="http://www.thebigwhammy.com/assets/images/default.jpg" alt="Card image cap">
+                          <div class="card-block">
+                              <h4 class="card-title">Everest</h4>
+                              <p class="card-text">Everest Award is given to the player who achieved the highest score in a month, in any game week.</p>
 
+                          </div>
+                      </div>
                   </div>
               </div><!-- /.row -->
           </div>
